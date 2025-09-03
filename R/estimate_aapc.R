0c7d91eaa9227257eb42c5529b4e922d256289a7
Matthew T. Warkentin <warkentin@lunenfeld.ca>
2025-09-03 14:13:08 -0600

    Change arg names

--- R/estimate_aapc.R
+++ R/estimate_aapc.R
@@ -1,4 +1,4 @@
-estimate_aapc <- function(data, df, conf.level, opts) {
+estimate_aapc <- function(data, deg_free, conf_level, opts) {
   data |>
     dplyr::mutate(
       d = period_end - period_start,
@@ -10,15 +10,13 @@ estimate_aapc <- function(data, df, conf.level, opts) {
       aapc = (opts$inv_fun(sum(db) / (sum(d))) - 1) * 100,
       aapc_ci_lwr = (opts$inv_fun(
         opts$fun((aapc / 100) + 1) -
-          (stats::qt(1 - (1 - conf.level) / 2, df) * sqrt(sum(d^2 * var)))
+          (stats::qt(1 - (1 - conf_level) / 2, deg_free) * sqrt(sum(d^2 * var)))
       ) -
-        1) *
-        100,
+        1),
       aapc_ci_upr = (opts$inv_fun(
         opts$fun((aapc / 100) + 1) +
-          (stats::qt(1 - (1 - conf.level) / 2, df) * sqrt(sum(d^2 * var)))
+          (stats::qt(1 - (1 - conf_level) / 2, deg_free) * sqrt(sum(d^2 * var)))
       ) -
-        1) *
-        100
+        1)
     )
 }
--- R/estimate_apc.R
+++ R/estimate_apc.R
@@ -1,4 +1,4 @@
-estimate_apc <- function(x, knots, model, periods, df, conf.level, opts) {
+estimate_apc <- function(x, knots, model, periods, deg_free, conf_level, opts) {
   periods <- get_periods(x, knots)
 
   broom::tidy(model) |>
@@ -7,16 +7,18 @@ estimate_apc <- function(x, knots, model, periods, df, conf.level, opts) {
     dplyr::mutate(
       var = diag(stats::vcov(model))[-1],
       apc = (opts$inv_fun(estimate) - 1) * 100,
+      apc_se = opts$inv_fun(estimate) * std.error,
       apc_ci_lwr = (opts$inv_fun(
-        (estimate - (stats::qt(1 - (1 - conf.level) / 2, df) * std.error))
+        (estimate - (stats::qt(1 - (1 - conf_level) / 2, deg_free) * apc_se))
       ) -
         1) *
         100,
       apc_ci_upr = (opts$inv_fun(
-        (estimate + (stats::qt(1 - (1 - conf.level) / 2, df) * std.error))
+        (estimate + (stats::qt(1 - (1 - conf_level) / 2, deg_free) * apc_se))
       ) -
         1) *
-        100
+        100,
+      apc_pval = NA_real_ #stats::dt(estimate, deg_free)
     )
 }
 
